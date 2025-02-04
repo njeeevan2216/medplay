adceab456a089b9718967d76ce83e6f283e53acf
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-02-04 15:42:05 +0530

    commit

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def home():
         print("Error fetching songs:", e)
         songs = []
     
-    return render_template('index.html', songs=[])
+    return render_template('base.html', songs=[])
 
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('q', '')
     print(query)
     if not query:
-        return render_template('index.html', songs=None)
+        return render_template('base.html', songs=None)
     
     try:
         response = requests.get(f"{API_URL}/search/songs?query={query}")
@@ -33,14 +33,14 @@ def search():
         print("Error fetching search results:", e)
         songs = []
     
-    return render_template('index.html', songs=songs)
+    return render_template('base.html', songs=songs)
 
 @app.route('/searchit', methods=['GET'])
 def searchit():
     query = request.args.get('q', '')
     print(query)
     if not query:
-        return render_template('index.html', songs=[])
+        return render_template('base.html', songs=[])
     
     try:
         response = requests.get(f"{API_URL}/search?query={query}")
@@ -51,7 +51,7 @@ def searchit():
         print("Error fetching search results:", e)
         songs = []
     
-    return render_template('index.html', songs=songs)
+    return render_template('base.html', songs=songs)
 
 if __name__ == '__main__':
     app.run(debug=True)
