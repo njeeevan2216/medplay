aa135bae32802d6b0d3a75d6432b50b8f7faf075
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-02-07 21:19:25 +0530

    Themes- full implementation
    themes has been completely implemented. with drop down menu for the themes

--- static/js/themer.js
+++ static/js/themer.js
@@ -1,3 +1,4 @@
+let isUp = true;
 fetch(`/static/json/themes.json`)
             .then(response => response.json())
             .then(themes => {
@@ -5,10 +6,12 @@ fetch(`/static/json/themes.json`)
                 themes.forEach(theme => {
                     const themeDiv = document.createElement('div');
                     themeDiv.classList.add('theme', theme.className);
+                    themeDiv.style.background = `${theme.colors[2]}`;
                     themeDiv.onclick = () => changeTheme(theme.colors[0], theme.colors[1], theme.colors[2], theme.colors[3]);
                     
                     const themeName = document.createElement('span');
                     themeName.classList.add('theme-name');
+                    themeName.style.color = `${theme.colors[0]}`;
                     themeName.textContent = theme.name;
                     themeDiv.appendChild(themeName);
                     
@@ -19,6 +22,7 @@ fetch(`/static/json/themes.json`)
                         colorDiv.classList.add('theme-color');
                         colorDiv.style.background = color;
                         colorBox.appendChild(colorDiv);
+                        colorDiv.style.border = `1px solid ${theme.colors[0]}`;
                     });
                     themeDiv.appendChild(colorBox);
                     
@@ -31,4 +35,22 @@ function changeTheme(c1, c2, c3, c4) {
     document.documentElement.style.setProperty('--back-dark', c2);
     document.documentElement.style.setProperty('--card-back', c3);
     document.documentElement.style.setProperty('--out-accent', c4);
+}
+
+function dropTheme() {
+
+    let themeCunt = document.querySelector(".theme-container");
+    let themeIco = document.getElementById("theme-icon");
+    
+    if (isUp) {
+        themeCunt.style.transform = "translateY(0px)";
+        isUp = false;
+        themeIco.classList.replace('fa-caret-down','fa-caret-up');
+    }
+    else {
+        themeCunt.style.transform = "translateY(-130px)";
+        isUp = true;
+        themeIco.classList.replace('fa-caret-up','fa-caret-down');
+    }
+
 }
