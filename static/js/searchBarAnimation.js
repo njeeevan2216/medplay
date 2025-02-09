e822102eeccf4bbf838ef2dd157297bfe17d1538
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-02-09 21:24:02 +0530

    js problem

--- static/js/searchBarAnimation.js
+++ static/js/searchBarAnimation.js
@@ -1,9 +1,3 @@
-let divSelected = document.getElementById("player-box");
-let divStyle = divSelected.style;
-let colorChosen = divStyle.getPropertyValue("background-color");
-console.log(colorChosen);
-console.log("heyy");
-
 document.addEventListener("DOMContentLoaded", function () {
     let input = document.querySelector(".input");
     let box = document.querySelector(".box-search");
@@ -12,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     box.addEventListener("mouseenter", function () {
         if (!expanded) {
             input.style.width = "350px";
-            input.style.background = `${colorChosen}`;
             input.style.borderRadius = "10px";
             expanded = true;
         }
